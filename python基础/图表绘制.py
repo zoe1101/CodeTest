3eda3415d5bcd8fd9f985b1ab8d8a927ddf0535e
tangli <tangli.1994@163.com>
2023-05-06 18:17:13 +0800

    commit #45864349

---
+++ python基础/图表绘制.py
@@ -0,0 +1,51 @@
+'''
+常见图表：
+条形图
+柱状图
+折线图
+饼图
+散点图
+面积图
+曲面图
+圆环图
+雷达图
+气泡图
+股价图
+'''
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+class ChartGeneration():
+    def bar_chart(self):
+        '''
+        条形图
+        :return:
+        '''
+        pass
+
+    def histogram(self):
+        '''
+        柱状图
+        :return:
+        '''
+        pass
+
+    def line_chart(self):
+        '''
+        折线图
+        :return:
+        '''
+        pass
+
+    def pie(self):
+        '''
+        饼图
+        :return:
+        '''
+        pass
+
+
+if __name__ == '__main__':
+    pass
