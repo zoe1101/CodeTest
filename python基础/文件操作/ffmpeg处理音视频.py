b104198313d033d6f1bb9692ff98cb9e1b7af212
tangli <tangli.1994@163.com>
2023-05-22 16:56:42 +0800

    commit #45864349

--- python基础/文件操作/ffmpeg处理音视频.py
+++ python基础/文件操作/ffmpeg处理音视频.py
@@ -1,4 +1,6 @@
 # coding:utf-8
+import os
+
 import ffmpeg
 
 '''
@@ -26,3 +28,19 @@ out, _ = (
     .output('../../data/01_shell初步_out3.mp4')
     .run(overwrite_output=True)
 )
+
+# 命令行写法
+'''
+其中：
+   -i 表示源视频文件
+   -y 表示输出文件，如果已存在则覆盖
+
+   -ss 表示从哪个时间点开始截取
+   -to 表示从哪个时间点结束截取
+
+   -vcodec copy 使用跟原视频相同的视频编解码器
+   -acodec copy 使用跟原视频相同的音频编解码器
+
+'''
+out_file = '../../data/01_shell初步_out4.mp4'
+os.system(f'ffmpeg -i {video_file} -ss 00:00:30 -to 00:00:40 -vcodec copy -acodec copy {out_file} -y')
